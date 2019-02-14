f51e600759fcd7e2126eaa98412dcd43b9a27d31
serbelga <serbelga@inf.upv.es>
2019-02-14 21:03:38 +0100

    Thumbnails in slider

--- KinectMultimediaPlayer/VideoPlayer.xaml.cs
+++ KinectMultimediaPlayer/VideoPlayer.xaml.cs
@@ -38,64 +38,6 @@ namespace KinectMultimediaPlayer
 
         }
 
-        /*
-        private void setTimeLine(Uri uri)
-        {
-            MediaPlayer mediaPlayer = new MediaPlayer();
-
-            
-            mediaPlayer.ScrubbingEnabled = true;
-            mediaPlayer.Open(uri);
-            mediaPlayer.Position = TimeSpan.FromSeconds(50);
-            mediaPlayer.MediaOpened += new EventHandler(mediaplayer_OpenMedia);
-        }
-
-        private void mediaplayer_OpenMedia(object sender, EventArgs e)
-        {
-            //----------------< mediaplayer_OpenMedia() >---------------- 
-            //*create mediaplayer in memory and jump to position 
-            //< draw video_image > 
-            MediaPlayer mediaPlayer = sender as MediaPlayer;
-            
-            DrawingVisual drawingVisual = new DrawingVisual();
-            DrawingContext drawingContext = drawingVisual.RenderOpen();
-            drawingContext.DrawVideo(mediaPlayer, new Rect(0, 0, 160, 100));
-            drawingContext.Close();
-
-            double dpiX = 1 / 200;
-            double dpiY = 1 / 200;
-            RenderTargetBitmap bmp = new RenderTargetBitmap(160, 100, dpiX, dpiY, PixelFormats.Pbgra32);
-            bmp.Render(drawingVisual);
-            //</ draw video_image > 
-
-            //< set Image > 
-            Image newImage = new Image();
-            newImage.Source = bmp;
-            newImage.Stretch = Stretch.Uniform;
-            newImage.Width = 100;
-            //</ set Image > 
-
-            //< add > 
-            KinectTileButton button = new KinectTileButton();
-            button.Label = "test";
-
-            button.Width = 200;
-            button.Height = 200;
-            button.FontSize = 18.0;
-            button.Content = newImage;
-            //button.Tag = videos[i].FullName.ToString();
-
-            scrollContent.Children.Insert(0, button);
-
-
-            button.Click += (o, re) =>
-            {
-                mediaElement.Position = TimeSpan.FromSeconds(50);
-            };
-            //</ add > 
-            //----------------< mediaplayer_OpenMedia() >---------------- 
-        }*/
-
         private void setTimeLine(Uri uri)
         {
             positions = new List<double>();
